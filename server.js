b226d2b0c13f006acb2b6d01f293bf0609451a29
Filipe Laborde-Basto <codefreeze8@gmail.com>
2020-06-09 14:15:46 -0400

    Next step

--- server.js
+++ server.js
@@ -2,9 +2,11 @@
  * Please initialize npm first (if NO package.json available):
  * npm init
  * npm install ascii-cats
- * 
+ * npm install chalk
  */
 const asciiCats = require('ascii-cats')
+const chalk = require('chalk')
+//import chalk from "chalk" // SAME as require
 
 let name = "Fil"
 
@@ -20,3 +22,13 @@ console.log( `The date is: ${getTime()}` )
 // run our new npm package called 'ascii-cats'
 const myCatAsciiImage = asciiCats()
 console.log( myCatAsciiImage )
+
+
+
+console.log( `...............................` )
+
+// string concatenation - template literals
+console.log(`${chalk.blue("Hello")} World${chalk.red("!")}`);
+ 
+// chainable API
+console.log(chalk.blue.bgRed.bold("Hello world!"));
