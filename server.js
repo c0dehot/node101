4d7c561c02701f2c7842d40b876efcd2dc62f461
Filipe Laborde-Basto <codefreeze8@gmail.com>
2020-06-09 13:57:00 -0400

    Next step

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
+/**
+ * Please initialize npm first (if NO package.json available):
+ * npm init
+ * npm install ascii-cats
+ * 
+ */
+const asciiCats = require('ascii-cats')
+
 let name = "Fil"
 
 console.log( `Hey ${name}, I am Node`)
@@ -7,5 +15,8 @@ console.log( `...............................` )
 function getTime(){
     return new Date()
 }
-
 console.log( `The date is: ${getTime()}` )
+
+// run our new npm package called 'ascii-cats'
+const myCatAsciiImage = asciiCats()
+console.log( myCatAsciiImage )
