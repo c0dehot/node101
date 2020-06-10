4cef67191c5ad2075fa36391295d11f095617349
Filipe Laborde-Basto <codefreeze8@gmail.com>
2020-06-10 12:10:01 -0400

    Added file read

---
+++ fileread.js
@@ -0,0 +1,12 @@
+const fs = require('fs') // fs | http 
+
+function readComplete( err, data ){
+    if( err ){
+        console.log( `Sorry something went wrong.` )
+        return;
+    }
+
+    console.log( `Finished reading: '${data}'` )
+}
+
+fs.readFile('README.md', 'utf8', readComplete)
