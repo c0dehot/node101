dfd9acd4a8e24d3571a974e2fdf38a2c1390f32c
Filipe Laborde-Basto <codefreeze8@gmail.com>
2020-06-11 13:07:25 -0400

    Waitfor more

--- waitfor.js
+++ waitfor.js
@@ -20,21 +20,21 @@ function waitFor(seconds) {
   }
   
   console.log( `-----------------------------------`)
-  console.log( `${showTime()} waitFor(5).then starting...`)
-  waitFor(5).then( function(result){
+console.log( `${showTime()} waitFor(5).then starting...`)
+waitFor(5).then( function(result){
     console.log( `${showTime()} .then has completed after 5s` )
-  })
-  
-  console.log( `${showTime()} waitFor(2).then starting...`)
-  waitFor(2).then( function(result){
-    console.log( `${showTime()} .then has completed after 2s` )
-  })
+})
 
-  async function mainApp(){
+console.log( `${showTime()} waitFor(2).then starting...`)
+waitFor(2).then( function(result){
+    console.log( `${showTime()} .then has completed after 2s` )
+})
+
+async function mainApp(){
     let result = await waitFor(5)
     console.log( `${showTime()} await 5-seconds has completed` )
     result = await waitFor(2)
     console.log( `${showTime()} await another 2-seconds has completed` )
-  }
-  console.log( `${showTime()} mainApp() starting...`)
-  mainApp()
+}
+console.log( `${showTime()} mainApp() starting...`)
+mainApp()
