aad0bf9f099c99ec39f4896740f44bd7b4bf3573
Filipe Laborde-Basto <codefreeze8@gmail.com>
2020-06-16 11:46:39 -0400

    Added editing for starwars characters

--- starwars.js
+++ starwars.js
@@ -5,6 +5,13 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+// look in 'html' FIRST and serve any static file
+app.use(express.static('html'))
+// app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+//
 // Data
 // ===========================================================
 const characters = {
@@ -30,23 +37,24 @@ const characters = {
 
 // Routes
 // ===========================================================
-app.get("/", function(req, res) {
+function checkAuthorization(req,res,next ){
+  console.log( `[checkAuthorization] called ` )
+  if( req.url !== '/' ){
+    res.send("Sorry you can't go there")
+    return
+  }
+  next()
+}
+
+app.get(["/","/hobbies","/edit"], checkAuthorization, function(req, res) {
   console.log( `[/ called] showing data ` )
   res.send("Welcome to the Star Wars Page!");
 });
 
-// to change a characters age
-app.get("/character/:name/:age", function(req, res) {
-  const name = req.params.name 
-  // /characters/obiwan/5000
-  if( characters.hasOwnProperty(name) ){
-    characters[name].age = req.params.age;
-    console.log( `age changed for: `, characters[name] )
-  }
-  res.send( { name, message: `Changed age to ${req.params.age}` } )
-});
-
 app.get("/character/:name", function(req, res) {
+  req.params = {
+    name: "obiwan"
+  }
   // /characters/obiwan/5000
   const name = req.params.name 
   console.log( `[/${name} called] showing ${name} object data ` )
@@ -56,6 +64,31 @@ app.get("/character/:name", function(req, res) {
     res.send( { error: "No character found" } )
   }
 });
+
+// to change a characters age
+app.get("/character/:name/:age", function(req, res) {
+  const name = req.params.name 
+  const age = req.params.age
+  // /characters/obiwan/5000
+  if( characters.hasOwnProperty(name) ){
+    characters[name].age = req.params.age;
+    console.log( `age changed for: `, characters[name] )
+  }
+  res.send( { name, message: `Changed age to ${req.params.age}` } )
+});
+
+app.post( "/api/edit-character", function( req, res ){
+  // req.body
+  console.log( req )
+
+  const newCharacter = req.body
+  console.log( `newCharacter: `, newCharacter )
+
+  characters[newCharacter.name] = newCharacter
+  console.log( `characters: `, characters )
+
+  res.send( { status: true, message: `Cool beans, we save it for you` } )
+})
 // Listener
 // ===========================================================
 app.listen(PORT, function() {
